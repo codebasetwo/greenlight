74f162c6215f1c96d9065a2aa30e92159c0241a3
Nnaemeka Nwankwo <codebasetwo@gmail.com>
2025-12-07 00:49:32 +0100

    handling json
    using struct tags and changing keys and defining directives

---
+++ cmd/api/example.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) _(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"hello": "world",
+	}
+	// Set the "Content-Type: application/json" header on the response.
+	w.Header().Set("Content-Type", "application/json")
+	// Use the json.NewEncoder() function to initialize a json.Encoder instance that
+	// writes to the http.ResponseWriter. Then we call its Encode() method, passing in
+	// the data that we want to encode to JSON (which in this case is the map above). If
+	// the data can be successfully encoded to JSON, it will then be written to our
+	// http.ResponseWriter.
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request",
+			http.StatusInternalServerError)
+	}
+}
