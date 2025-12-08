9b393e74629c85b934f76ce67bde590662c742a1
Nnaemeka Nwankwo <codebasetwo@gmail.com>
2025-12-08 01:03:52 +0100

    envelope responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,10 +6,12 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a map which holds the information that we want to send in the response.
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 	// Pass the map to the json.Marshal() function. This returns a []byte slice
 	// containing the encoded JSON. If there was an error, we log it and send the client
