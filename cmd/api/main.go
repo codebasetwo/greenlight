e4d15d701c1c78daf3c5b37c31286ffb3d25bc02
Nnaemeka Nwankwo <codebasetwo@gmail.com>
2025-12-18 21:25:18 +0100

    cross origin requests
    implemented simple and preflight cors.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -40,6 +41,10 @@ type config struct {
 		password string
 		sender   string
 	}
+	// Add a cors struct and trustedOrigins field with the type []string.
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -72,6 +77,17 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "api", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "8276deb42dd3112d0d8487888dcab6fd", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "hello@demomailtrap.co", "SMTP sender")
+
+	// Use the flag.Func() function to process the -cors-trusted-origins command line
+	// flag. In this we use the strings.Fields() function to split the flag value into a
+	// slice based on whitespace characters and assign it to our config struct.
+	// Importantly, if the -cors-trusted-origins flag is not present, contains the empty
+	// string, or contains only whitespace, then strings.Fields() will return an empty
+	// []string slice.
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
